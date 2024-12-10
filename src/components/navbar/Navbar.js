3e9954962d76d464b9884ff9b897d88401b8c394
Rilwanu Idris <Rillygulma@gmail.com>
2024-12-10 22:37:21 +0100

    Display Nav items when click on icon on mobile devices

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 import { navLinksdata } from '../../constants';
 
 function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <div className="w-full h-20 sticky top-0 z-50 bg-bodyColor flex justify-between items-center px-4 md:px-10 lg:px-20 font-titleFont border-b-[1px] border-b-gray-600">
       {/* Logo */}
@@ -12,6 +18,7 @@ function Navbar() {
 
       {/* Navigation Links */}
       <div>
+        {/* Desktop Menu */}
         <ul className="hidden md:flex items-center gap-6 lg:gap-10">
           {navLinksdata.map(({ _id, title, link }) => (
             <li
@@ -31,11 +38,42 @@ function Navbar() {
             </li>
           ))}
         </ul>
-        {/* Mobile Menu */}
+
+        {/* Mobile Menu Toggle */}
         <div className="md:hidden flex items-center text-gray-400">
-          {/* Add mobile menu toggle logic or icon here if required */}
-          <span className="text-lg cursor-pointer hover:text-designColor duration-300">☰</span>
+          <span
+            className="text-lg cursor-pointer hover:text-designColor duration-300"
+            onClick={handleMenuToggle}
+          >
+            ☰
+          </span>
         </div>
+
+        {/* Mobile Menu */}
+        {isMobileMenuOpen && (
+          <div className="absolute top-20 left-0 w-full bg-bodyColor border-t-[1px] border-t-gray-600 md:hidden">
+            <ul className="flex flex-col items-center gap-6 py-4">
+              {navLinksdata.map(({ _id, title, link }) => (
+                <li
+                  className="text-sm font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300"
+                  key={_id}
+                  onClick={handleMenuToggle} // Close menu on link click
+                >
+                  <Link
+                    activeClass="active"
+                    to={link}
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                  >
+                    {title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
